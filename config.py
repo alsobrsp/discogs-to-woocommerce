5a1250e737ecf3e45fda8f8a200c461cad3f9eed
Syd Alsobrook <syd@seasies.com>
2017-08-25 07:01:02 -0400

    Better product formating / Create product

--- config.py
+++ config.py
@@ -11,7 +11,8 @@ wooconfig = {'url': "https://www.decadesofvinyl.com",
                         'consumer_key': "ck_f839dfe156f0253a7b4a7cd810a40de86d1b1519",
                         'consumer_secret': "cs_220b8c6746a0733df9c73c656ef699db8baaceca",
                         'wp_api': True,
-                        'version': "wc/v2"
+                        'version': "wc/v2", 
+                        'timeout': 20
                         }
 
 # Discogs API setup
@@ -19,4 +20,4 @@ UserAgent = 'DoV/0.1 +https://www.decadesofvinyl.com'
 AuthToken = "DiSiupFPDVYxsOqpOtwjXmfENbNeLNfhhaCYqbso"
 
 # NOTE: This is the index not the id. folder 0 for ALL, 26 for 7up test
-discogs_folder=27
+discogs_folder=0
--- process_woo.py
+++ process_woo.py
@@ -10,6 +10,7 @@ import sys
 import dbqueries as dbq
 # import hashlib
 from woocommerce import API
+import htmlgen
 
 # Import config
 from config import *
@@ -28,14 +29,17 @@ def main():
     # Get woo attributes, name to id mapping
     woo_attributes_list = get_woo_attributes_list()
     # TODO: Genres to attributes
-    update_attrib_term_list('genre', woo_attributes_list['Genre'])
+    update_attrib_term_list('genres', woo_attributes_list['Genre'])
 
     # TODO: create catagories
     
     # TODO: create new products
-#    db_new_products = dbq.exec_db_query_dict(dbq.get_new_woo_instances,  qty="all")
-#    create_new_products(db_new_products)
+    db_new_products = dbq.exec_db_query_dict(dbq.get_new_woo_instances,  qty="all")
+    create_new_products(db_new_products)
+    # TODO: group multiple products? SELECT release_id, title, COUNT(*) copies FROM dov_discogs_instances GROUP BY release_id HAVING copies > 1;
     # TODO: update / reactivate existing products 
+    # TODO: Cross/Up sell items
+    # TODO: Group items
     # TODO: deactivate removed products
     # TODO: Sold products Woo -> Discogs
     sys.exit(0)
@@ -51,34 +55,186 @@ def updateWooProduct(instance_id):
     pass
 
 # Create Product
-def create_new_products (db_new_products):
+def create_new_products(db_new_products):
     """
     Take list of new instance id's, create product page and update instance table with Woo ID
     """
     for idx in range(len(db_new_products)):
         # Get instance data from db
         instance_data = dbq.exec_db_query_dict(dbq.get_instance_info,  db_new_products[idx]['instance_id'])
-        # Get release data from db
-        release_data = dbq.exec_db_query_dict(dbq.get_release_info, instance_data['release_id'])
+
+        # Get release data from Discogs
+        # release_data = dbq.exec_db_query_dict(dbq.get_release_info, instance_data['release_id'])
+        release_data = discogs.release(db_new_products[idx]['release_id'])
+
+        # Send data to formater
+        product_data = formatproduct(instance_data, release_data)
+
+        # Create Woo Product
+        product = wcapi.post("products", product_data).json()
         
-        pp.pprint(instance_data)
-        pp.pprint(release_data)
-        
-        
+        # Update DB with Woo Product ID
+        query_data = {"woo_id":  product['id'], 
+                                "instance_id": db_new_products[idx]['instance_id']}
+        query = dbq.update_instance_woo_id
+        dbq.exec_db_query(query, query_data, query_type='insert')
+
+def format_generic(object):
+    """
+    Works for artists, companies, credits, labels
+    """
+    html_string = ""
+    for idx in range(len(object)):
+        html_string += str(htmlgen.Link(object[idx].url,  object[idx].name))
+        if 'catno' in dir(object[idx]):
+            html_string += ': {0}\n'.format(object[idx].catno)
+        else:
+            html_string += "\n"
+    return html_string
     
-    # Create Woo Product
-#   data = {
-#        "name": "Premium Quality",
-#        "type": "simple",
-#        "regular_price": "21.99",
-#        "description": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
-#        "short_description": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-#    }
-#    
-#   product = wcapi.post("products", data).json()
+def format_formats(object):
+    string = ""
+    for idx in range(len(object)):
+        string = (object[idx]['name'] + ", " + ', '.join(object[idx]['descriptions']))
+    return string
     
-def formatproduct():
-    pass
+def format_identifiers(object):
+    string = ""
+    for idx in range(len(object)):
+        type = object[idx]['type']
+        string += '{0} '.format(type)
+        if 'description' in object[idx]:
+            desc = object[idx]['description']
+            string += '({0})'.format(desc)
+        if 'value' in object[idx]:
+            value = object[idx]['value']
+            string += ': {0}\n'.format(value)
+    return string
+
+def format_image_array(object):
+    images = []
+    for idx in range(len(object)):
+        images.append({"src": object[idx]['uri'], "position": idx})
+    return images
+
+def format_instance_notes(object):
+    notes = ""
+    object = eval(object)
+    for idx in range(len(object)):
+        if object[idx]['field_id'] == 1:
+            notes += "Media Condition: {0}\n".format(object[idx]['value'])
+        if object[idx]['field_id'] == 2:
+            notes += "Sleeve Condition: {0}\n".format(object[idx]['value'])
+        if object[idx]['field_id'] == 3:
+            notes += "Decades of Vinyl Notes\n{0}\n".format(object[idx]['value'])
+    return notes
+
+def formatproduct(instance_data, release_data):
+    """
+    This will take the instance, release, artist, and label data to build the product page
+    TODO: Will need an object processor function, there are a number of array/object lists.
+    """
+    # Artists list
+    artists = format_generic(release_data.artists)
+    # Company list
+    companies = format_generic(release_data.companies)
+    # Extra Artists / Credits
+    credits = format_generic(release_data.credits)
+    # Formats
+    formats = format_formats(release_data.formats)
+    # TODO: move to attibutes
+    genres = str(release_data.genres)
+    # Identifiers - barcodes, runouts and such
+    identifiers = format_identifiers(release_data.identifiers)
+    # Labels
+    labels = format_generic(release_data.labels)
+    # TODO: move to attibutes    
+    styles = str(release_data.styles)
+    # TODO: some traks have extraartists in credits
+#    tracklist = release_data.tracklist
+    # Discogs release URL
+    url = "<a href=\"" + release_data.url + "\"_blank\">Discogs release info</a>"
+    # TODO: Embed videos
+#    videos = release_data.videos
+
+    short_description = format_instance_notes(instance_data['notes'])
+
+    description = ("This is a first pass on the data import. The format will improve.\n"
+                            "Artists\n" + artists + "\n"
+                            "Country of Release: " + str(release_data.country) + "\n"
+                            "Companies\n" + companies + "\n"
+                            "Credits\n" + credits + "\n"
+                            "Formats\n" + formats + "\n"
+                            "Genres: " + genres + " / This will moved to searchable attributes\n"
+                            "Identifiers\n" + identifiers + "\n"
+                            "Labels\n" + labels + "\n"
+                            "Release Date: " + str(release_data.released) + "\n"
+                            "Styles\n" + styles + "\n"
+#                            "Tracklist\n" + tracklist + "\n"
+                            "Discogs URL: " + url + "\n"
+#                            "Videos\n" + videos + "\n"
+                            "Year: " + str(release_data.year) + "\n"
+                            "\n"
+                            "Discogs release notes:\n" + str(release_data.notes) + "\n"
+                            "**************\n"
+                            "All data pulled from Discogs"
+                            )
+
+    images = format_image_array(release_data.images)
+    
+    # Genres, ....
+    attributes = [{}]
+    
+    # TODO: First pass will only use index 0 for any entry. Should be updated to show all where appropriate
+    data = {"name": release_data.artists[0].name + " - " + release_data.title, 
+                  "description": description, 
+                  "short_description": str(short_description), 
+                  "sku": str(instance_data['instance_id']), 
+                  "images": images, 
+                  "attributes": attributes}
+    '''
+    name	string	Product name.
+    slug	string	Product slug.
+    type	string	Product type. Options: simple, grouped, external and variable. Default is simple.
+    status	string	Product status (post status). Options: draft, pending, private and publish. Default is publish.
+    featured	boolean	Featured product. Default is false.
+    catalog_visibility	string	Catalog visibility. Options: visible, catalog, search and hidden. Default is visible.
+    description	string	Product description.
+    short_description	string	Product short description.
+    sku	string	Unique identifier.
+    regular_price	string	Product regular price.
+    sale_price	string	Product sale price.
+    date_on_sale_from	date-time	Start date of sale price, in the site’s timezone.
+    date_on_sale_from_gmt	date-time	Start date of sale price, as GMT.
+    date_on_sale_to	date-time	End date of sale price, in the site’s timezone.
+    date_on_sale_to_gmt	date-time	End date of sale price, in the site’s timezone.
+    external_url	string	Product external URL. Only for external products.
+    button_text	string	Product external button text. Only for external products.
+    tax_status	string	Tax status. Options: taxable, shipping and none. Default is taxable.
+    tax_class	string	Tax class.
+    manage_stock	boolean	Stock management at product level. Default is false.
+    stock_quantity	integer	Stock quantity.
+    in_stock	boolean	Controls whether or not the product is listed as “in stock” or “out of stock” on the frontend. Default is true.
+    backorders	string	If managing stock, this controls if backorders are allowed. Options: no, notify and yes. Default is no.
+    sold_individually	boolean	Allow one item to be bought in a single order. Default is false.
+    weight	string	Product weight.
+    dimensions	object	Product dimensions. See Product - Dimensions properties
+    shipping_class	string	Shipping class slug.
+    reviews_allowed	boolean	Allow reviews. Default is true.
+    upsell_ids	array	List of up-sell products IDs.
+    cross_sell_ids	array	List of cross-sell products IDs.
+    parent_id	integer	Product parent ID.
+    purchase_note	string	Optional note to send the customer after purchase.
+    categories	array	List of categories. See Product - Categories properties
+    tags	array	List of tags. See Product - Tags properties
+    images	object	List of images. See Product - Images properties
+    attributes	array	List of attributes. See Product - Attributes properties
+    default_attributes	array	Defaults variation attributes. See Product - Default attributes properties
+    menu_order	integer	Menu order, used to custom sort products.
+    meta_data	array	Meta data. See Product - Meta data properties
+    '''
+    
+    return data
     
 def get_woo_attributes_list():
     woo_attributes_list = {}
